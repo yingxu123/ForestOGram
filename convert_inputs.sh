5bf8547895a5c30ec54372ba0eb9deac19f4461f
Russell Jarvis <russelljjarvis@protonmail.com>
2022-06-15 15:32:44 +1000

    add rafs onset and offset times

--- convert_inputs.sh
+++ convert_inputs.sh
@@ -10,3 +10,8 @@ for f in *\ *; do mv "$f" "${f// /_}"; done
 for file in *.mp3; do
    ffmpeg -i "$file" -acodec pcm_s16le -ac 1 -ar 44100 "${file%.mp3}".wav
 done
+##
+# move the mp3 files into an mp3 directory
+##
+for f in *.mp3; do mv "$f" "mp3/${f}"; done
+for f in *.wav; do mv "$f" "wav/${f}"; done
