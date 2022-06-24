f3d64cc88b1b7052084a622b0806079605bc29c4
Russell Jarvis <russelljjarvis@protonmail.com>
2022-06-24 17:29:09 +1000

    update notebook and tests

--- python/tests/tests.py
+++ python/tests/tests.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.insert(0,"../")
 sys.path.insert(0,"./")
-sys.path.insert(0,"/home/runner/work/ForestOGram/python/*")
+sys.path.insert(0,"/home/runner/work/ForestOGram/python")
+sys.path.insert(0,"/home/runner/work/ForestOGram/python/ProcessSegments")
 print(sys.path)
 import ProcessSegments
 def test_import():
