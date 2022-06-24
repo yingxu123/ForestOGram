75dddd6a057c5e3043a9b1772f4f4a04a6068f20
Russell Jarvis <russelljjarvis@protonmail.com>
2022-06-24 13:36:55 +1000

    run tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,9 @@
 #from src.math import add, subtract, multiply
 def test_import():
     import ProcessSegments
-    assert type(ProcessSegments) not None
+    assert not type(ProcessSegments) is None
 
 def test_methods():
     import ProcessSegments
     segments = ProcessSegments.process_data()
-    assert type(segments) not None
+    assert not type(segments) is None
