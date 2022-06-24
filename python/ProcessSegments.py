e5bac44d05e2d29bce740ada49aeba5576d9d918
Russell Jarvis <russelljjarvis@protonmail.com>
2022-06-24 13:32:06 +1000

    update

--- python/ProcessSegments.py
+++ python/ProcessSegments.py
@@ -20,23 +20,29 @@ import wave
 
 import numpy as np
 
-path ="../mp3/XC155388-Pink Pigeon (song, Mauritius, Black River Gorge, nov2012, 2).MP3"
-sound = AudioSegment.from_file(file=path)
-fs = sound.frame_rate
-left = sound.split_to_mono()[1]
-bit_depth = left.sample_width * 8
-array_type = get_array_type(bit_depth)
-numeric_array = array.array(array_type, left._data)
-label = loadtxt("../onset_offset/XC155388 - Pink Pigeon - Nesoenas mayeri.txt")
 
-for ind,lab in enumerate(label):
-    onset = int(lab[0]*fs)
-    offset = int(lab[1]*fs)
-    segment = numeric_array[onset:offset]
-    write("segment_%s.wav" %raw, fs, int16(segment))
-    print ("finish generating segment %s is from %s s (%s) to %s (%s)s" %(raw,lab[0], onset,lab[1], offset) )
-
-
+def load_data():
+    path ="../mp3/XC155388-Pink Pigeon (song, Mauritius, Black River Gorge, nov2012, 2).MP3"
+    sound = AudioSegment.from_file(file=path)
+    fs = sound.frame_rate
+    left = sound.split_to_mono()[1]
+    bit_depth = left.sample_width * 8
+    array_type = get_array_type(bit_depth)
+    numeric_array = array.array(array_type, left._data)
+    labels = loadtxt("../onset_offset/XC155388 - Pink Pigeon - Nesoenas mayeri.txt")
+    return (fs,numeric_array,sound,labels)
+def process_data():
+    fs,numeric_array,sound,labels = load_data()
+    segments = []
+    for ind,lab in enumerate(labels):
+        onset = int(lab[0]*fs)
+        offset = int(lab[1]*fs)
+        segment = numeric_array[onset:offset]
+        segments.append(segment)
+        write("../wav/segment_%s.wav" %ind, fs, int16(segment))
+        print ("finish generating segment %s is from %s s (%s) to %s (%s)s" %(ind, lab[0], onset,lab[1], offset) )
+    return segments
+segments = process_data()
 """
 old code
 num_label = np.shape(label)[0]
---
+++ python/tests/tests.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.insert(0,"./")
+print(sys.path)
+import ProcessSegments
+def test_import():
+    assert not type(ProcessSegments) is None
+
+def test_methods():
+    segments = ProcessSegments.process_data()
+    assert not type(segments) is None
