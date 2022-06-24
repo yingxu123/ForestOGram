f5d135f1463bde777d2200d9fe5d9bffd37fb4a5
Russell Jarvis <russelljarvis@protonmail.com>
2022-06-24 18:20:34 +1000

    Delete tests.py

--- python/tests/tests.py
+++
@@ -1,8 +0,0 @@
-import sys
-import ProcessSegments
-def test_import():
-    assert not type(ProcessSegments) is None
-
-def test_methods():
-    segments = ProcessSegments.process_data()
-    assert not type(segments) is None
