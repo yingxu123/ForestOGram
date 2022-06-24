83ee1a0944c8fd61574466deee6a81542dc37e1e
Russell Jarvis <russelljjarvis@protonmail.com>
2022-06-24 13:47:08 +1000

    run tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,10 @@
-#from src.math import add, subtract, multiply
+import sys
+sys.path.insert(0,"./python")
+print(sys.path)
+import ProcessSegments
 def test_import():
-    import ProcessSegments
     assert not type(ProcessSegments) is None
 
 def test_methods():
-    import ProcessSegments
     segments = ProcessSegments.process_data()
     assert not type(segments) is None
